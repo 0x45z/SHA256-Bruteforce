9218696d6162c2169a029bad2b0e21531baaf461
Adam Copley <copleya2@uni.coventry.ac.uk>
2021-10-20 17:42:39 +0100

    Added function to pass all hashes through the decryption function and returning an output of the hash, plaintext and time taken. This function will also display the number of hashes cracked and the total time taken for all of the hashes to be cracked. All of the cracked hashes are then formatted and displayed along with the plain text and the time taken to crack each hash before the program exits.

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class bruteForce():
         for attempt in bruteForce.crack(current_char, max_length): # for each attempt in crack function
             if bruteForce.hash(attempt) == passw_hash: # compare hashed value of guess with hash value
                 time_taken = ('{:.2f}s'.format(time.time() - self.time)) # get time taken to complete within 2 decimal places
-                return passw_hash, attempt, time_taken # return hash, plain text password and time taken
-        return passw_hash, 'Character set exhausted. Password not found.', ('{:.2f}s'.format(time.time() - self.time)) # if password not found
+                return passw_hash, attempt, time_taken, True # return hash, plain text password and time taken
+        return passw_hash, 'Character set exhausted. Password not found.', ('{:.2f}s'.format(time.time() - self.time)), False # if password not found
 
     @staticmethod
     def crack(charset=char1+char3+char4,maxlength=5):
@@ -44,6 +44,34 @@ def get_csv_hashes():
             csv_hashes.append(lines['hash']) # save hashes in csv to the list csv_hashes
     return csv_hashes
 
+def main():
+    start_time = time.time() # get start time
+    results = {}
+    cracked = 0
+    print('[Status]: %s Hashes received\n' % len(hashes))
+    for hash in hashes:
+        print(f'[Status]: Attempting to decrypt hash {hash}\n')
+        data = bruteForce().main(hash) # pass hash to bruteForce class and save returned values
+        if data[3] == True: # if hash successfully cracked
+            print(f'[Cracked]: {data[0]}:{data[1]} Time elapsed: {data[2]}\n') # print the hash, plaintext and time taken
+            cracked+=1 # increment the counter
+        else:
+            print(f'[Error]: {data[0]} {data[1]} Time elapsed: {data[2]}\n')
+        results[data[0]] = list() # create list in dictionary with the password hash as key
+        results[data[0]].extend(data[1:]) # add the rest of the data to the dictionary under the hash as the key
+    total_taken = ('{:.2f}s'.format(time.time() - start_time)) # get total time elapsed to 2 dp
+    print('[Status]: Process complete.\n')
+    print(f'Cracked {cracked}/{len(hashes)} hashes in {total_taken}\n')
+    print('Hash                              Plaintext        Time taken')
+    for hash in hashes:
+        if not results[hash][2]: # if hash not cracked
+            continue
+        print(hash[:30]+'...', results[hash][0], (' '*(15-len(results[hash][0]))),results[hash][1])
+        """
+        ^^ print first 30 characters of hash, plaintext value, the appropriate amount of spaces so that
+        the value of time taken aligns with the time taken column and finally time taken.
+        this is done so that the text is formatted for the generic console window size.
+        """
 
 
 
@@ -83,4 +111,4 @@ if __name__ == "__main__":
         if 's' in args.charset:
             current_char += char4 # add special characters charset to current charset
 
-    print(bruteForce().main(hashes[0]))
+    main()
